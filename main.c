c583d87d6029f543567dfa9b68d7c598fbc8bd26
Nginze <kuugjonathan45@gmail.com>
2024-03-25 22:32:42 +0000

    alpha commit

---
+++ main.c
@@ -0,0 +1,19 @@
+/***************************************************************************//**
+  @file         main.c
+  @author       Jonathan Kuug & Abdul Wahab Abass 
+  @date         April 2024 
+  @brief        DSH (Dash Shell - Minimal Shell)
+*******************************************************************************/
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
